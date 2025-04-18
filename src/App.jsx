48cdcab7612ab9578f322dbf8832cfe4babbb355
Olena Surilova <elen21surilova@gmail.com>
2025-04-18 19:30:46 +0200

    feat: implement zooming functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import './App.css';
+import  { useState } from 'react';
+
+import Controls from './components/Controls';
 
 function App() {
- 
+  const [frameZoom, setFrameZoom] = useState(false);
+
+  const toggleZoom = () => {
+    setFrameZoom(prevState => {
+      return !prevState;
+    })
+  }
 
   return (
-    <>
-      <h1>Start</h1>
-    </>
+    <div className='w-full h-screen grid place-items-center'>
+      <div className={`${frameZoom && 'min-w-[97vw] min-h-[97vh]'} w-[70vw] h-[85vh] min-w-[70vw] min-h-[85vh] max-w-[90vw] max-h-[90vh] border border-gray-300 rounded-2xl resize overflow-auto relative transition-all duration-100`}>
+        <Controls toggleZoom={toggleZoom} frameZoom={frameZoom} />
+      </div>
+    </div>
   )
 }
 
