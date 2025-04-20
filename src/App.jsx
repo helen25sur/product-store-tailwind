2a0024063603b3eb3d2cbdaa808360924841a28e
Olena Surilova <elen21surilova@gmail.com>
2025-04-20 18:28:38 +0200

    feat: add nav-bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import './App.css';
 import  { useState } from 'react';
 
 import Controls from './components/Controls';
+import Navbar from './components/Navbar';
 
 function App() {
   const [frameZoom, setFrameZoom] = useState(false);
+  const [activePage, setActivePage] = useState(0);
 
   const toggleZoom = () => {
     setFrameZoom(prevState => {
@@ -12,9 +14,14 @@ function App() {
     })
   }
 
+  const handleNavPage = (pageIndex) => {
+    setActivePage(pageIndex);
+  }
+
   return (
     <div className='w-full h-screen grid place-items-center'>
       <div className={`${frameZoom && 'min-w-[97vw] min-h-[97vh]'} w-[70vw] h-[85vh] min-w-[70vw] min-h-[85vh] max-w-[90vw] max-h-[90vh] border border-gray-300 rounded-2xl resize overflow-auto relative transition-all duration-100`}>
+        <Navbar activePage={activePage} handleNavPage={handleNavPage} />
         <Controls toggleZoom={toggleZoom} frameZoom={frameZoom} />
       </div>
     </div>
